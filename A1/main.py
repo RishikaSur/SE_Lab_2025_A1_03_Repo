831a82c2b680b35a9ad69a10fcdea86506660c24
SayantanSinha03 <sayantansinha480@gmail.com>
2025-04-01 21:54:32 +0530

    Create main.py

--- A1/main.py
+++ A1/main.py
@@ -1,55 +1,30 @@
-import sqlite3
-from typing import Generator, Tuple
+import seller as slr
+import buyer as byr
+from db import inventory as inv
 
-class Inventory:
-    conn = sqlite3.connect("inventory.db")
-    cursor = conn.cursor()
+while True:
+    ch = int(
+        input(
+            """
+Welcome to the Inventory
 
-    def __init__(self):
-        with self.conn:
-            self.cursor.execute(
-                "CREATE TABLE IF NOT EXISTS INV(PROD_NAME STRING PRIMARY KEY, QUANT INTEGER, COST REAL)"
-            )
+1) Buyer
+2) Seller
+3) View inventory
+4) Exit
 
-    def addItem(self, name, quant, cost):
-        with self.conn:
-            self.cursor.execute(
-                "INSERT INTO INV VALUES('" + name + "', '" + str(quant) + "', '" + str(cost) + "')"
-            )
+Enter choice: """
+        )
+    )
+    if ch == 1:
+        byr.show()
+    elif ch == 2:
+        slr.show()
+    elif ch == 3:
+        inv.show()
+    elif ch == 4:
+        exit()
+    else:
+        print("Bad Choice. Try again")
+        continue
 
-    def removeItem(self, name):
-        with self.conn:
-            self.cursor.execute(
-                "DELETE FROM INV WHERE PROD_NAME='" + name + "'"
-            )
-
-    def updateItem(self, name, quant=None, cost=None):
-        with self.conn:
-            if quant is not None:
-                self.cursor.execute(
-                    "UPDATE INV SET QUANT=" + str(quant) + " WHERE PROD_NAME='" + name + "'"
-                )
-            if cost is not None:
-                self.cursor.execute(
-                    "UPDATE INV SET COST=" + str(cost) + " WHERE PROD_NAME='" + name + "'"
-                )
-
-    def getItems(self):
-        for row in self.cursor.execute("SELECT * FROM INV"):
-            yield row
-
-    def getItem(self, name):
-        return self.cursor.execute(
-            "SELECT * FROM INV WHERE PROD_NAME='" + name + "'"
-        ).fetchone()
-
-    def show(self):
-        print("\nInventory List:")
-        print("-" * 30)
-        print("Product Name | Quantity | Cost per Item")
-        print("-" * 30)
-        for row in self.getItems():
-            print(row[0] + " | " + str(row[1]) + " | " + str(row[2]))
-        print("-" * 30)
-
-inventory = Inventory()
